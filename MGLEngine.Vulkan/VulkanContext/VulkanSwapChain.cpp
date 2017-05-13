0f227cacb7f035a7ca3bbff4fb71b72fbaabed42
alcoforado <marcosalcoforado@gmail.com>
2017-05-13 12:27:13 -0400

    Create image views

--- MGLEngine.Vulkan/VulkanContext/VulkanSwapChain.cpp
+++ MGLEngine.Vulkan/VulkanContext/VulkanSwapChain.cpp
@@ -43,15 +43,46 @@ VulkanSwapChain::VulkanSwapChain(const VulkanSurface& surface, const VulkanLogic
 	auto err=vkCreateSwapchainKHR(device.GetHandle(), &createInfo, nullptr, &_handle);
 	AssertVulkanSuccess(err);
 
+
+	//Get The imageds handlers
 	_imageFormat = createInfo.imageFormat;
 	_imageColorSpace = createInfo.imageColorSpace;
-
+	_dims = createInfo.imageExtent;
 	uint32_t imagesCount;
 	vkGetSwapchainImagesKHR(_logicalDevice.GetHandle(), _handle, &imagesCount, nullptr);
 	_images.resize(imagesCount);
 	vkGetSwapchainImagesKHR(_logicalDevice.GetHandle(), _handle, &imagesCount, _images.data());
 
 
+	//Get images view;
+	for(auto img : _images)
+	{
+	
+		VkImageViewCreateInfo createViewInfo={};
+		createViewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		createViewInfo.image = img;
+		createViewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		createViewInfo.format = _imageFormat;
+		createViewInfo.flags = 0;
+		createViewInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createViewInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createViewInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createViewInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+		
+		//no layers or mipmaps
+		createViewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		createViewInfo.subresourceRange.baseMipLevel = 0;
+		createViewInfo.subresourceRange.levelCount = 1;
+		createViewInfo.subresourceRange.baseArrayLayer = 0;
+		createViewInfo.subresourceRange.layerCount = 1;
+	
+		VkImageView view;
+		err=vkCreateImageView(device.GetHandle(), &createViewInfo, nullptr, &view);
+		AssertVulkanSuccess(err);
+
+		_imagesviews.push_back(view);
+	
+	}
 
 
 
--- MGLEngine.Vulkan/VulkanContext/VulkanSwapChain.h
+++ MGLEngine.Vulkan/VulkanContext/VulkanSwapChain.h
@@ -8,10 +8,11 @@ class VulkanSwapChain
 	const VulkanSurface&  _surface;
 	const VulkanLogicalDevice& _logicalDevice;
 	VkSwapchainKHR _handle;
-	int _imageFormat;  
+	VkFormat _imageFormat;  
 	int _imageColorSpace; 
-
-	std::vector<VkImage> _images; 
+	VkExtent2D _dims;
+	std::vector<VkImage> _images;
+	std::vector<VkImageView> _imagesviews;
 	public:
 	VulkanSwapChain(const VulkanSurface &surface,const  VulkanLogicalDevice &device);
 	~VulkanSwapChain();
