1f213a31e314dfce383750e73718787cb0a7b64f
alcoforado <marcosalcoforado@gmail.com>
2017-05-24 23:22:23 -0400

    ADDING IARRAY

--- MGLEngine.Shared/Utils/Arrays/IArray.h
+++ MGLEngine.Shared/Utils/Arrays/IArray.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <stddef.h>
+#ifndef IARRAY_H
+#define IARRAY_H
+#include<assert.h>
+template <class OfType>
+class IArray
+{
+
+protected:
+	IArray() { _ptr = NULL; _size = 0; }
+	OfType *_ptr;
+	Index _size;
+public:
+
+	OfType& operator[](Index i)
+	{
+		assert(i<_size);
+		return _ptr[i];
+	}
+	Index size() { return _size; }
+
+	OfType* GetPointer() { return _ptr; }
+};
+
+#endif // IARRAY_H
