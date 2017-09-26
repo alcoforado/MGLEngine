f1f019fe02e88e168b0b915bc80a273ea5fe70f4
mmendes <mmendes@isc2.org>
2017-09-26 16:52:04 -0400

    Installing Microsoft Owin Static Files Adding Startup

---
+++ MGLEngine.Server/App_Start/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Microsoft.Practices.Unity;
+using Owin;
+using Unity.WebApi;
+
+namespace TestApp.App_Config
+{
+    public class Startup
+    {
+        internal static IUnityContainer Container { get; set; }
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { action = "index", id = RouteParameter.Optional }
+            );
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+            config.DependencyResolver = new UnityDependencyResolver(Container);
+            appBuilder.UseWebApi(config);
+
+
+            var fileSystem = new PhysicalFileSystem("../../../content");
+            var options = new FileServerOptions
+            {
+                EnableDirectoryBrowsing = true,
+                FileSystem = fileSystem
+            };
+
+            appBuilder.UseFileServer(options);
+
+
+        }
+    }
+}
