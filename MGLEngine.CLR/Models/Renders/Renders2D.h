d7d17831fffde50418b2e801f1bda0a8e52440dc
mmendes <mmendes@isc2.org>
2017-09-29 13:58:25 -0400

    Creating CyclicColor Render (Managed)

---
+++ MGLEngine.CLR/Models/Renders/Renders2D.h
@@ -0,0 +1,10 @@
+#pragma once
+#using "NetLibs/GlmNet.dll"
+using namespace GlmNet;
+using namespace System::Collections::Generic;
+public ref class CyclicColor
+{
+public:
+	List<vec3> Colors;
+
+};
