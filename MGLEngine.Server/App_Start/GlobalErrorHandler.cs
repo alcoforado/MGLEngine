abfcfb3e568a7e45e9bcd058b71b90472d0265e6
alcoforado <marcosalcoforado@gmail.com>
2017-12-12 19:41:09 -0500

    Create error handler for the web api server to format user exceptions. format is {    Errors: ["error1".error"] }

--- MGLEngine.Server/App_Start/GlobalErrorHandler.cs
+++ MGLEngine.Server/App_Start/GlobalErrorHandler.cs
@@ -1,18 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
 
 namespace MGLEngine.Server.App_Start
 {
+    public class ErrorResult
+    {
+        public List<string> Errors = new List<string>();
+    }
+
     class GlobalErrorHandler : ExceptionHandler
     {
         public override void Handle(ExceptionHandlerContext context)
         {
-            context.
+            
+            var errorResult = new ErrorResult();
+
+            var ex = context.Exception;
+            do
+            {
+                var userException = ex as UserException;
+                if (userException != null)
+                {
+                    errorResult.Errors.AddRange(userException.Errors);
+                }
+                else
+                {
+                    errorResult.Errors.Add(context.Exception.Message);
+                }
+                ex = ex.InnerException;
+            } while (ex != null);
+
+
+
+            context.Result= new FormattedContentResult<ErrorResult>(
+                HttpStatusCode.BadRequest,
+                errorResult, 
+                new JsonMediaTypeFormatter(), 
+                new MediaTypeHeaderValue("application/json"),
+                context.Request);
             base.Handle(context);
         }
 
---
+++ MGLEngine.Server/App_Start/UserException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGLEngine.Server.App_Start
+{
+    public class UserException : Exception
+    {
+        public List<string> Errors { get; private set; }
+
+        UserException()
+        {
+            Errors = new List<string>();
+        }
+
+        UserException AddError(string error)
+        {   
+            Errors.Add(error);
+            return this;
+        }
+
+        UserException AddErrors(List<string> error)
+        {
+            if (error != null)
+                Errors.AddRange(error);
+            return this;
+        }
+
+
+    }
+}
