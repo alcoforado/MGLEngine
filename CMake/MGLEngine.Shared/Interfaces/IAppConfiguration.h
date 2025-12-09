304f3e996a70d3ebbb9d9a6644f961bed7065dd3
Marcos Mendes <MMendes@ConnectWise.com>
2025-12-09 13:38:27 -0500

    t

--- CMake/MGLEngine.Shared/Interfaces/IAppConfiguration.h
+++ CMake/MGLEngine.Shared/Interfaces/IAppConfiguration.h
@@ -1,14 +1,15 @@
 #pragma once
 #include <string>
-#include <unordered_map>
+#include <map>
 #include <MGLEngine.Shared/Interfaces/IShader.h>
-
+#include <typeinfo>
+#include <typeindex>
 
 class IAppConfiguration {
 	public:
 		virtual void EnableDebugLayer(bool flag) = 0;
 		virtual void AppName(std::string name) = 0;
-		virtual void AddShader(std::string id, IShader* pShader) = 0;
+		virtual void AddShader(IShader* pShader) = 0;
 		virtual void SetDoubleBuffer() = 0;
 		virtual void SetTrippleBuffer() = 0;
 		virtual void EnableVSync(bool flag) = 0;
@@ -18,14 +19,14 @@ class AppConfiguration : public IAppConfiguration {
 
 public:
 	std::string Name;
-	std::unordered_map<std::string, IShader*> Shaders;
+	std::map<std::type_index, IShader*> Shaders;
 	bool EnableDebug = false;
 	unsigned SwapChainSize = 0; //number of images buffers in the swap chain
 	bool VSync = true;
 public:
 	virtual void EnableDebugLayer(bool flag) { EnableDebug = flag; }
 	virtual void AppName(std::string name) { Name = name; }
-	virtual void AddShader(std::string id, IShader* pShader) { Shaders[id] = pShader; }
+	virtual void AddShader(IShader* pShader) { Shaders[id] = pShader; }
 	virtual void SetDoubleBuffer() { SwapChainSize = 2; }
 	virtual void SetTrippleBuffer() { SwapChainSize = 3; }
 	virtual void EnableVSync(bool flag) { VSync = flag; }
